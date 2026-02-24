ad20d85b19d9953316533ad50413d77adc073d2d
mxou <maximilien.dlrm@gmail.com>
2026-02-24 02:32:31 +0100

    Ajout addTodo, deleteTodo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from 'vue'
+import HeaderDate from './components/HeaderDate.vue'
+
+const todosStore = ref([])
+const todo = ref('')
+
+function addTodo() {
+  if (!todo.value) return
+  todosStore.value.push(todo.value)
+  todo.value = ''
+}
+
+function deleteTodo(index) {
+  todosStore.value.splice(index, 1)
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <HeaderDate />
+  <div class="todo_form">
+    <input v-model="todo" type="text" placeholder="Ajouter une tâche.." />
+    <button @click="addTodo()">Ajouter</button>
+    <ul>
+      <li v-for="(todo, index) in todosStore" :key="index">
+        {{ todo }} <button @click="deleteTodo(index)">Sup</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/HeaderDate.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({})
+</script>
+
+<template>
+  <header>
+    <div class="number_date_container">
+      <h1 class="date">12</h1>
+      <div class="text_date">
+        <p>JAN</p>
+        <p>2026</p>
+      </div>
+    </div>
+    <p>MARDI</p>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.number_date_container {
+  /* border: 2px solid black; */
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+}
+
+.date {
+  font-size: 3rem;
+}
+</style>
